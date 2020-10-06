3df5a4ddb0921fc0583cbed691d32ca160e981c4
NamCV <namcv@sendo.vn>
2020-10-06 15:14:49 +0700

    log serviceName

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
 const Console = require('console').Console;
 const consoleWrapper = new Console(process.stdout, process.stderr);
 class ConsoleClass {
-    static log(inStream1, inStream2) {
-        let now = new Date();
-        if(inStream2){
-            inStream2 = `[${now}] ${JSON.stringify(inStream2)}`;
-            consoleWrapper.log(inStream1,inStream2);
-        } else {
-            inStream1 = `[${now}] ${JSON.stringify(inStream1)}`;
-            consoleWrapper.log(inStream1)
-        }
-    }
+  constructor(serviceName) {
+    this.serviceName = serviceName.toUpperCase();
+  }
 
-    static info(inStream1, inStream2) {
-        let now = new Date();
-        if(inStream2){
-            inStream2 = `[INFO] [${now}] ${JSON.stringify(inStream2)}`;
-            consoleWrapper.log(inStream1,inStream2);
-        } else {
-            inStream1 = `[INFO] [${now}] ${JSON.stringify(inStream1)}`;
-            consoleWrapper.log(inStream1)
-        }
+  static log(inStream1, inStream2) {
+    let now = new Date();
+    if (inStream2) {
+      inStream2 = `[${now}] ${JSON.stringify(inStream2)}`;
+      consoleWrapper.log(`${serviceName} ${inStream1}`, inStream2);
+    } else {
+      inStream1 = `[${now}] ${JSON.stringify(inStream1)}`;
+      consoleWrapper.log(inStream1)
     }
+  }
 
-    static warn(inStream1, inStream2) {
-        let now = new Date();
-        if(inStream2){
-            inStream2 = `[WARN] [${now}] ${JSON.stringify(inStream2)}`;
-            consoleWrapper.log(inStream1,inStream2);
-        } else {
-            inStream1 = `[WARN] [${now}] ${JSON.stringify(inStream1)}`;
-            consoleWrapper.log(inStream1)
-        }
+  static info(inStream1, inStream2) {
+    let now = new Date();
+    if (inStream2) {
+      inStream2 = `[INFO] [${now}] ${JSON.stringify(inStream2)}`;
+      consoleWrapper.info(`${serviceName} ${inStream1}`, inStream2);
+    } else {
+      inStream1 = `[INFO] [${now}] ${JSON.stringify(inStream1)}`;
+      consoleWrapper.info(inStream1)
     }
+  }
 
-    static error(inStream1, inStream2) {
-        let now = new Date();
-        if(inStream2){
-            inStream2 = `[ERROR] [${now}] ${JSON.stringify(inStream2)}`;
-            consoleWrapper.log(inStream1,inStream2);
-        } else {
-            inStream1 = `[ERROR] [${now}] ${JSON.stringify(inStream1)}`;
-            consoleWrapper.log(inStream1)
-        }
+  static warn(inStream1, inStream2) {
+    let now = new Date();
+    if (inStream2) {
+      inStream2 = `[WARN] [${now}] ${JSON.stringify(inStream2)}`;
+      consoleWrapper.warn(`${serviceName} ${inStream1}`, inStream2);
+    } else {
+      inStream1 = `[WARN] [${now}] ${JSON.stringify(inStream1)}`;
+      consoleWrapper.warn(inStream1)
     }
+  }
 
-    static debug(inStream1, inStream2) {
-        let now = new Date();
-        if(inStream2){
-            inStream2 = `[DEBUG] [${now}] ${JSON.stringify(inStream2)}`;
-            consoleWrapper.log(inStream1,inStream2);
-        } else {
-            inStream1 = `[DEBUG] [${now}] ${JSON.stringify(inStream1)}`;
-            consoleWrapper.log(inStream1)
-        }
+  static error(inStream1, inStream2) {
+    let now = new Date();
+    if (inStream2) {
+      inStream2 = `[ERROR] [${now}] ${JSON.stringify(inStream2)}`;
+      consoleWrapper.error(`${serviceName} ${inStream1}`, inStream2);
+    } else {
+      inStream1 = `[ERROR] [${now}] ${JSON.stringify(inStream1)}`;
+      consoleWrapper.error(inStream1)
     }
+  }
+
+  static debug(inStream1, inStream2) {
+    let now = new Date();
+    if (inStream2) {
+      inStream2 = `[DEBUG] [${now}] ${JSON.stringify(inStream2)}`;
+      consoleWrapper.debug(`${serviceName} ${inStream1}`, inStream2);
+    } else {
+      inStream1 = `[DEBUG] [${now}] ${JSON.stringify(inStream1)}`;
+      consoleWrapper.debug(inStream1)
+    }
+  }
 }
 
 exports.console = ConsoleClass;
